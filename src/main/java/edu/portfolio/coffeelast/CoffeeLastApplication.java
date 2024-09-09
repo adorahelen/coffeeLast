7c609578f8f2378b4d6a409041c36f9d6ab3e367
adorahelen <adorahelenmin@gmail.com>
2024-09-09 14:21:01 +0900

    Initial commit

---
+++ src/main/java/edu/portfolio/coffeelast/CoffeeLastApplication.java
@@ -0,0 +1,13 @@
+package edu.portfolio.coffeelast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeLastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeLastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/portfolio/coffeelast/CoffeeLastApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.portfolio.coffeelast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeLastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
